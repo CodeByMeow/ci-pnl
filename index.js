f23c16964776b459cf74860cfc98faca9750ec63
Thien Khanh <tranquanki28@gmail.com>
2022-04-12 14:58:59 +0700

    resolve propblem 1 - 4

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//PNL - CIJS - TRAN THIEN KHANH
 //----------Proplem 1-----------//
 function wrapHtml(users) {
+  if (!Array.isArray(users)) return `Input must be array`;
   return users.map(
     ({ name, age }) => `<div><h1>${name}</h1><h2>${age}</h2></div>`
   );
@@ -13,10 +15,12 @@ const users = [
   { name: "Bob Ziroll", age: 100 },
 ];
 
-// console.log(wrapHtml(users));
+console.log(`Proplem 1:`);
+console.log(wrapHtml(users));
 
 //----------Proplem 2-----------//
 function filterGender(students) {
+  if (!Array.isArray(students)) return `Input must be array`;
   return students.filter(({ gender }) => gender !== "female");
 }
 
@@ -27,14 +31,47 @@ const students = [
   { name: "Peter", gender: "other" },
 ];
 
-// console.log(filterGender(students));
+console.log(`Proplem 2:`);
+console.log(filterGender(students));
 
 //----------Proplem 3-----------//
 function groupAnagram(strs) {
-  if (!Array.isArray(strs)) return `Wrong input`;
-  const temp = strs.map(word => {
-    //TODO: working here
-  }
+  if (!Array.isArray(strs)) return `Input must be array`;
+  const temp = strs.map((word) => {
+    const value = word;
+    const strsort = word.split("").sort().join("");
+    return { sort: strsort, data: value };
+  });
+  const result = temp.reduce((r, item) => {
+    r[item.sort] = r[item.sort] || [];
+    r[item.sort].push(item.data);
+    return r;
+  }, {});
+  return Object.values(result);
 }
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
-console.log(groupAnagram(strs));
+const t1 = ["eat", "tea", "tan", "ate", "nat", "bat"];
+const t2 = [""];
+const t3 = ["a"];
+const t4 = "this is a string";
+
+console.log(`Proplem 3:`);
+console.log(groupAnagram(t1));
+
+//----------Proplem 4-----------//
+function findSingleNumber(arr) {
+  if (!Array.isArray(arr)) return `Input must be array`;
+  const counter = {};
+  arr.forEach((number) => {
+    counter[number] = counter[number] || 0;
+    return ++counter[number];
+  });
+  const result = Object.keys(counter).find((key) => counter[key] === 1);
+  return result;
+}
+
+const nums1 = [2, 2, 1];
+const nums2 = [4, 1, 2, 1, 2];
+const nums3 = [1];
+
+console.log(`Proplem 4:`);
+console.log(findSingleNumber(nums1));
